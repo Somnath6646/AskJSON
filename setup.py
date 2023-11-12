e3e1c3aebe67cfc4ccaf7f889ae75c9df9bd9fb7
rudrodip <official.rudrodipsarker@gmail.com>
2023-11-12 17:08:18 +0600

    removed unnecessary requirements

--- setup.py
+++ setup.py
@@ -12,18 +12,15 @@ setup(
         'openai',
         'termcolor',
         'python-dotenv',
-        'langchain',
         'stdlib-list',
-        'python-Levenshtein',  # If this is a required dependency, it should be listed here.
     ],
-    author='Your Name',  # Replace with your name
-    author_email='your.email@example.com',  # Replace with your email address
+    author='Your Name',
+    author_email='your.email@example.com',
     description='A library to ask questions to JSON data and get Python code in response.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/somnath6646/AskJSON',  # Replace with the actual URL to your project repository
+    url='https://github.com/somnath6646/AskJSON',
     classifiers=[
-        # Classifiers help users find your project by categorizing it.
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
@@ -32,12 +29,11 @@ setup(
         "Topic :: Utilities"
     ],
     python_requires='>=3.6',
-    keywords='json query ai openai python code generation',  # Add some relevant keywords
-    project_urls={  # Optional
-        'Bug Tracker': 'https://github.com/yourusername/AskJSON/issues',
-        'Documentation': 'https://github.com/yourusername/AskJSON#readme',
-        'Source Code': 'https://github.com/yourusername/AskJSON',
-        # Add any other relevant links
+    keywords='json query ai openai python code generation',
+    project_urls={
+        'Bug Tracker': 'https://github.com/Somnath6646/AskJSON/issues',
+        'Documentation': 'https://github.com/Somnath6646/AskJSON#readme',
+        'Source Code': 'https://github.com/Somnath6646/AskJSON',
     },
     include_package_data=True,  # Includes files specified by MANIFEST.in
     # Optional settings:
